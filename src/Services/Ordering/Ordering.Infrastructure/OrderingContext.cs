46ca7c1f33452f4743b444b3436106a54380e729
Zhang Jian <110623298@qq.com>
2021-03-22 21:40:19 +0800

    use SeedWork.Infrastructure

--- src/Services/Ordering/Ordering.Infrastructure/OrderingContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/OrderingContext.cs
@@ -7,105 +7,24 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage;
 using ZhangJian.YunFeiShop.Services.Ordering.Domain.AggregatesModel.OrderAggregate;
-using ZhangJian.YunFeiShop.BuildingBlocks.SeedWork.Domain;
+using ZhangJian.YunFeiShop.BuildingBlocks.SeedWork.Infrastructure;
 
 namespace ZhangJian.YunFeiShop.Services.Ordering.Infrastructure
 {
-    public class OrderingContext : DbContext, IUnitOfWork
+    public class OrderingContext : DbContextBase
     {
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderLine> OrderLines { get; set; }
 
-        private readonly IMediator _mediator;
-        private IDbContextTransaction _currentTransaction;
-        public bool HasActiveTransaction => _currentTransaction != null;
-        public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
-
         public OrderingContext(DbContextOptions<OrderingContext> options) : base(options) { }
 
-        public OrderingContext(DbContextOptions<OrderingContext> options, IMediator mediator) : base(options)
-        {
-            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-
-
-            System.Diagnostics.Debug.WriteLine("OrderingContext::ctor ->" + this.GetHashCode());
-        }
+        public OrderingContext(DbContextOptions<OrderingContext> options, IMediator mediator) : base(options, mediator) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new OrderEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new OrderLineEntityTypeConfiguration());
         }
-
-        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
-        {
-            // Dispatch Domain Events collection. 
-            // Choices:
-            // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
-            // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
-            // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
-            // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
-            await _mediator.DispatchDomainEventsAsync(this);
-
-            // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
-            // performed through the DbContext will be committed
-            var result = await base.SaveChangesAsync(cancellationToken);
-
-            return true;
-        }
-
-        public async Task<IDbContextTransaction> BeginTransactionAsync()
-        {
-            if (_currentTransaction != null) return null;
-
-            _currentTransaction = await Database.BeginTransactionAsync(IsolationLevel.ReadCommitted);
-
-            return _currentTransaction;
-        }
-
-        public async Task CommitTransactionAsync(IDbContextTransaction transaction)
-        {
-            if (transaction == null) throw new ArgumentNullException(nameof(transaction));
-            if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
-
-            try
-            {
-                await SaveChangesAsync();
-                transaction.Commit();
-            }
-            catch
-            {
-                RollbackTransaction();
-                throw;
-            }
-            finally
-            {
-                if (_currentTransaction != null)
-                {
-                    _currentTransaction.Dispose();
-                    _currentTransaction = null;
-                }
-            }
-        }
-
-        public void RollbackTransaction()
-        {
-            try
-            {
-                _currentTransaction?.Rollback();
-            }
-            finally
-            {
-                if (_currentTransaction != null)
-                {
-                    _currentTransaction.Dispose();
-                    _currentTransaction = null;
-                }
-            }
-        }
-
-
-
     }
 
     class OrderEntityTypeConfiguration : IEntityTypeConfiguration<Order>
